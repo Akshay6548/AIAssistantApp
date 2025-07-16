bd6872ee4632cd81943b8ede1287f073eaec185e
Akshay6548 <akshaysudigala2012@gmail.com>
2025-07-16 15:26:14 -0400

    Add files via upload

--- StreamlitUI.py
+++ StreamlitUI.py
@@ -1,5 +1,29 @@
+import firebase_admin
+import google.generativeai as genai
 import streamlit as st
 
+# Access Firebase credentials as a dictionary
+firebase_creds = dict(st.secrets["firebase"])
+
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate(firebase_creds)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+
+
+# Access root-level secrets
+gemini_key = st.secrets["gemini_api_key"]
+
+
+genai.configure(gemini_key)
+model = genai.GenerativeModel("gemini-pro")
+
+def get_response(prompt):
+    response = model.generate_content(prompt)
+    return response.text
 st.title("Personal AI Assistant")
 
 user_input = st.text_input("Ask me anything:")
